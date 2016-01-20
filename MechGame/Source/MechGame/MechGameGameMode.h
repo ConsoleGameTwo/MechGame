c6ba296d164065b58cd9a40c7f3496c76026a47f
unknown <Tech@C401IT09.SCT.local>
2016-01-20 10:52:47 -0500

    first push

---
+++ MechGame/Source/MechGame/MechGameGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "MechGameGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MECHGAME_API AMechGameGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
